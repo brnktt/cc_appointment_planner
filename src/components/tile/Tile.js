2e08fe258941d8acd90a3585e70f3ca762c54c64
Alessandro Branchetti <brk.alessandro@gmail.com>
2024-01-20 06:35:03 +0100

    added Tile component

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({ name, description }) => {
   return (
     <div className="tile-container">
-      
+      <p className="tile tile-title">{name}</p>
+      {Object.values(description).map((desc) => {
+        return (
+          <p className="tile">{desc}</p>
+        )
+      })}
     </div>
   );
 };
