6414b6aa7555ef82abb2a46310e123bdbf5ef298
Alessandro Branchetti <brk.alessandro@gmail.com>
2024-01-20 06:57:32 +0100

    cleaned code

--- src/App.js
+++ src/App.js
@@ -5,69 +5,37 @@ import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
 
 function App() {
-  const [contacts, setContacts] = useState([
-    {
-      name: "first contact",
-      phoneNumber: "123456789",
-      email: "firstcontact@gmail.com",
-    },
-    {
-      name: "second contact",
-      phoneNumber: "987654321",
-      email: "secondcontact@gmail.com",
-    },
-  ]);
-  const [appointments, setAppointments] = useState([
-    {
-      name: "first appointment",
-      contact: "first contact",
-      date: "24-01-01",
-      time: "16:00"
-    },
-    {
-      name: "second appointment",
-      contact: "second contact",
-      date: "27-01-01",
-      time: "14:00"
-    },
-  ]);
+  const [appointments, setAppointments] = useState([]);
+  const [contacts, setContacts] = useState([]);
 
-  function onContactAdd(name, phoneNumber, email) {
-    const newContact = {
-      name,
-      phoneNumber,
-      email,
-    };
-
-    setContacts((prev) => {
-      return [
-        ...prev,
-        newContact,
-      ];
-    });
+  const addAppointment = (name, contact, date, time) => {
+    setAppointments([
+      ...appointments,
+      {
+        name: name,
+        contact: contact,
+        date: date,
+        time: time,
+      },
+    ]);
   };
 
-  function onAppointmentAdd(name, contact, date, time) {
-    const newAppointment = {
-      name,
-      contact,
-      date,
-      time,
-    };
-
-    setAppointments((prev) => {
-      return [
-        ...prev,
-        newAppointment,
-      ];
-    });
+  const addContact = (name, phone, email) => {
+    setContacts([
+      ...contacts,
+      {
+        name: name,
+        phone: phone,
+        email: email,
+      },
+    ]);
   };
 
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={ <Root/> }>
       <Route index element={ <Navigate to={ROUTES.CONTACTS} replace/> }/>
-      <Route path={ROUTES.CONTACTS} element={ <ContactsPage contacts={contacts} onContactAdd={onContactAdd} /> }/>
-      <Route path={ROUTES.APPOINTMENTS} element={ <AppointmentsPage appointments={appointments} onAppointmentAdd={onAppointmentAdd} /> }/>
+      <Route path={ROUTES.CONTACTS} element={ <ContactsPage contacts={contacts} addContact={addContact} /> }/>
+      <Route path={ROUTES.APPOINTMENTS} element={ <AppointmentsPage appointments={appointments} addAppointment={addAppointment} /> }/>
     </Route>
   ));
   
